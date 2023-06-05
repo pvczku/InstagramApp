fe6f44a58ae06e963a24861bb4000fdc5e1f4c48
pvczku <patryk.kulma.dev@gmail.com>
2023-06-05 17:19:57 +0000

    :zap: Add styling
    li styling

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,42 +2,43 @@ import React from "react";
 import { Link as ReactLink } from "react-router-dom";
 import { Link } from "@chakra-ui/react";
 import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Navbar() {
   return (
     <>
       <ul>
-        <li>
+        <li style={{ display: "flex", alignItems: "center" }}>
           <HomeIcon />
           <Link as={ReactLink} to={"/home"}>
             Home
           </Link>
         </li>
-        <li>
-          <HomeIcon />
+        <li style={{ display: "flex", alignItems: "center" }}>
+          <SearchIcon />
           <Link as={ReactLink} to={"/home"}>
             Search
           </Link>
         </li>
-        <li>
+        <li style={{ display: "flex", alignItems: "center" }}>
           <HomeIcon />
           <Link as={ReactLink} to={"/home"}>
             Explore
           </Link>
         </li>
-        <li>
+        <li style={{ display: "flex", alignItems: "center" }}>
           <HomeIcon />
           <Link as={ReactLink} to={"/home"}>
             Messages
           </Link>
         </li>
-        <li>
+        <li style={{ display: "flex", alignItems: "center" }}>
           <HomeIcon />
           <Link as={ReactLink} to={"/home"}>
             Upload
           </Link>
         </li>
-        <li>
+        <li style={{ display: "flex", alignItems: "center" }}>
           <HomeIcon />
           <Link as={ReactLink} to={"/profile"}>
             Profile

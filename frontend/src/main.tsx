25dc4c1ac1650ac07fdbdc04995efba31e54797e
pvczku <patryk.kulma.work@gmail.com>
2023-06-13 19:42:55 +0200

    :zap: Add scss stylesheet

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-// import "./scss/main.scss";
+import "./scss/main.scss";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
